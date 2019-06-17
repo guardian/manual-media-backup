156efa8b78e8d5adbbb051eaa39dcb4bfa6b61a7
Andy Gallagher <andy.gallagher@theguardian.com>
2019-06-17 17:26:24 +0100

    improve readability of NDjson processing and improve error handling

--- src/main/scala/helpers/ListReader.scala
+++ src/main/scala/helpers/ListReader.scala
@@ -6,11 +6,13 @@ import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.model._
 import akka.http.scaladsl.model.headers.Accept
-import akka.stream.Materializer
-import akka.stream.scaladsl.{FileIO, Framing, Keep, Sink, Source}
+import akka.stream.{ClosedShape, Materializer, SinkShape}
+import akka.stream.scaladsl.{FileIO, Framing, GraphDSL, Keep, RunnableGraph, Sink, Source}
 import akka.util.ByteString
+import io.circe.Json
 import models.{Incoming, IncomingFilename, IncomingListEntry}
 import org.slf4j.LoggerFactory
+import streamcomponents.{MarshalJsonToIncoming, ParseJsonEntry}
 
 import scala.concurrent.{ExecutionContext, Future}
 import scala.util.{Failure, Success}
@@ -38,7 +40,7 @@ object ListReader {
     ).collect({case Some(hdr)=>hdr})
 
     Http().singleRequest(HttpRequest(HttpMethods.GET,uri,headers)).flatMap(response=>{
-      response.entity.dataBytes
+      response.entity.withoutSizeLimit().dataBytes
         .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = maxLinesize))
         .map(_.decodeString("UTF-8"))
         .toMat(Sink.fold[Seq[Incoming],String](Seq())((acc,entry)=>acc++Seq(IncomingFilename(entry))))(Keep.right)
@@ -53,24 +55,53 @@ object ListReader {
     })
   }
 
+  /**
+    * returns an akka stream sink to decode an incoming stream of newline-delimited JSON
+    * this needs to be hooked into a ByteString producing source, presumably from Akka http
+    */
+  def ndJsonDecodeSink = {
+    val sinkFactory = Sink.fold[Seq[Incoming],IncomingListEntry](Seq())((acc,entry)=>acc++Seq(entry))
+
+    GraphDSL.create(sinkFactory) { implicit builder => sink=>
+      import akka.stream.scaladsl.GraphDSL.Implicits._
+
+      val framer = builder.add(Framing.delimiter(ByteString("\n"), maximumFrameLength = maxLinesize).map(_.decodeString("UTF-8")))
+      val parser = builder.add(new ParseJsonEntry().async)
+      val marshaller = builder.add(new MarshalJsonToIncoming().async)
+      framer ~> parser ~> marshaller ~> sink
+      SinkShape(framer.in)
+    }
+  }
+
+  def foldStringSink = {
+    Sink.reduce[ByteString]((acc, entry)=>acc.concat(entry))
+  }
+
+  /**
+    * receives a list of files to process as newline-delimited JSON from an HTTP(S) endpoint
+    * @param uri URI to receive from
+    * @param acceptContentType Optional content type to insist on from the server
+    * @param system implicitly provided Actor System
+    * @param mat implicitly provided Materializer
+    * @param ec implicitly provided Execution Context
+    * @return a Future, containing :
+    *         if the response is not 200 (Success), a Left with the content body as a UTF-8 string
+    *         if the response is 200 (Success), a Right with decoded IncomingListEntry objects.
+    */
   def fromHttpNDJson(uri:Uri,acceptContentType:Option[String])(implicit system:ActorSystem, mat:Materializer, ec:ExecutionContext) = {
     val headers:scala.collection.immutable.Seq[HttpHeader] = scala.collection.immutable.Seq(
       acceptContentType.map(ct=>new Accept(scala.collection.immutable.Seq(MediaRange(MediaType.custom(ct,false)))))
     ).collect({case Some(hdr)=>hdr})
 
     Http().singleRequest(HttpRequest(HttpMethods.GET,uri,headers)).flatMap(response=>{
-      response.entity.dataBytes
-        .via(Framing.delimiter(ByteString("\n"), maximumFrameLength = maxLinesize))
-        .map(_.decodeString("UTF-8"))
-        .toMat(Sink.fold[Seq[Incoming],String](Seq())((acc,entry)=>acc++Seq(IncomingFilename(entry))))(Keep.right)
-        .run()
-        .map(result=>{
-          if(response.status!=StatusCodes.OK){
-            Right(result)
-          } else {
-            Left(result.mkString("\n"))
-          }
+      if(response.status==StatusCodes.OK){
+        response.entity.withoutSizeLimit().dataBytes.toMat(ndJsonDecodeSink)(Keep.right).run().map(Right(_))
+      } else {
+        response.entity.withoutSizeLimit().dataBytes.toMat(foldStringSink)(Keep.right).run().map(bodyBytes=>{
+          val bodyString = bodyBytes.decodeString("UTF-8")
+          Left(bodyString)
         })
+      }
     })
   }
 
