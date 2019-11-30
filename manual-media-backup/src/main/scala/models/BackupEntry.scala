fbd2d7e5d7b9d10f694e3b05ffe847cc2c32d073
Andy Gallagher <andy.gallagher@theguardian.com>
2019-11-30 15:36:22 +0000

    implement a custom counter sink for initial work estimates, and building stats into BackupEntry

--- manual-media-backup/src/main/scala/models/BackupEntry.scala
+++ manual-media-backup/src/main/scala/models/BackupEntry.scala
@@ -2,4 +2,7 @@ package models
 
 import java.nio.file.Path
 
-case class BackupEntry(originalPath:Path, maybeObjectMatrixEntry:Option[ObjectMatrixEntry])
+case class BackupEntry(originalPath:Path,
+                       maybeObjectMatrixEntry:Option[ObjectMatrixEntry]=None,
+                       status:BackupStatus.Value=BackupStatus.NOT_STARTED,
+                       timelag:Option[Long]=None)
---
+++ manual-media-backup/src/test/scala/streamcomponents/TwoPortCounterSpec.scala
@@ -0,0 +1,32 @@
+package streamcomponents
+
+import akka.stream.{ActorMaterializer, ClosedShape, Materializer}
+import akka.stream.scaladsl.{GraphDSL, Merge, RunnableGraph, Source}
+import org.specs2.mutable.Specification
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+class TwoPortCounterSpec extends Specification {
+  "TwoPortCounter" should {
+    "Count incoming elements on both ports and materialize the final value" in new AkkaTestkitSpecs2Support {
+      val sinkFact = new TwoPortCounter[Int]
+
+      implicit val mat:Materializer = ActorMaterializer.create(system)
+      val graph = GraphDSL.create(sinkFact) { implicit builder=> sink=>
+        import akka.stream.scaladsl.GraphDSL.Implicits._
+
+        val src1 = Source.fromIterator(()=>Seq(1,2,3,4,5,6).toIterator)
+        val src2 = Source.fromIterator(()=>Seq(1,2,3,4).toIterator)
+        src1 ~> sink.inlets(0)
+        src2 ~> sink.inlets(1)
+
+        ClosedShape
+      }
+      Merge
+
+      val result = Await.result(RunnableGraph.fromGraph(graph).run(), 10 seconds)
+      result mustEqual CounterData(6,4)
+    }
+  }
+}
