6a1e7b813c771dc126ae21f23e89b88cc351eb9c
Andy Gallagher <andy.gallagher@theguardian.com>
2020-02-17 11:24:05 +0000

    fix for some masters failing to process due to null durations

--- common/src/main/scala/helpers/PlutoCommunicator.scala
+++ common/src/main/scala/helpers/PlutoCommunicator.scala
@@ -51,102 +51,104 @@ object PlutoCommunicator {
   case object LookupFailed extends AFHMsg
 }
 
-class PlutoCommunicator(plutoBaseUri:String, plutoUser:String, plutoPass:String)(implicit system:ActorSystem, mat:Materializer) extends Actor {
+class PlutoCommunicator(override val plutoBaseUri:String, override val plutoUser:String, override val plutoPass:String)
+                       (override implicit val system:ActorSystem, override val mat:Materializer) extends Actor with PlutoCommunicatorFuncs {
   import PlutoCommunicator._
-  private val logger = LoggerFactory.getLogger(getClass)
 
-  private var assetFolderCache:Map[Path, Option[AssetFolderRecord]] = Map()
-  private var projectsCache:Map[String,Option[ProjectRecord]] = Map()
-  private var commissionsCache:Map[String, Option[CommissionRecord]] = Map()
-  private var workingGroupCache:Map[UUID, Option[WorkingGroupRecord]] = Map()
-  private var masterCache:Map[String,Seq[MasterRecord]] = Map()
-  private var deliverableAssetCache:Map[String,Option[DeliverableAssetRecord]] = Map()
-  private var deliverableBundleCache:Map[Int,Option[DeliverableBundleRecord]] = Map()
+  override protected val logger = LoggerFactory.getLogger(getClass)
 
-  protected val ownRef:ActorRef = self
+  private var assetFolderCache: Map[Path, Option[AssetFolderRecord]] = Map()
+  private var projectsCache: Map[String, Option[ProjectRecord]] = Map()
+  private var commissionsCache: Map[String, Option[CommissionRecord]] = Map()
+  private var workingGroupCache: Map[UUID, Option[WorkingGroupRecord]] = Map()
+  private var masterCache: Map[String, Seq[MasterRecord]] = Map()
+  private var deliverableAssetCache: Map[String, Option[DeliverableAssetRecord]] = Map()
+  private var deliverableBundleCache: Map[Int, Option[DeliverableBundleRecord]] = Map()
+
+  protected val ownRef: ActorRef = self
 
   override def receive: Receive = {
-    case TestConnection=>
+    case TestConnection =>
       val originalSender = sender()
       performCommsCheck.onComplete({
-        case Success(_)=>originalSender ! ConnectionWorking
-        case Failure(err)=>
+        case Success(_) => originalSender ! ConnectionWorking
+        case Failure(err) =>
           logger.error(s"Could not establish communication with pluto: ", err)
           originalSender ! LookupFailed
       })
-    case StoreInCache(forPath, assetFolder)=>
-      assetFolderCache ++= Map(forPath->assetFolder)
+    case StoreInCache(forPath, assetFolder) =>
+      assetFolderCache ++= Map(forPath -> assetFolder)
       sender() ! akka.actor.Status.Success
-    case Lookup(forPath)=>
+    case Lookup(forPath) =>
       assetFolderCache.get(forPath) match {
-        case Some(assetFolder)=>
+        case Some(assetFolder) =>
           sender() ! FoundAssetFolder(assetFolder)
-        case None=>
+        case None =>
           val originalSender = sender()
           performAssetFolderLookup(forPath).onComplete({
-            case Success(assetFolder)=>
+            case Success(assetFolder) =>
               ownRef ! StoreInCache(forPath, assetFolder)
               originalSender ! FoundAssetFolder(assetFolder)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could not look up potential asset folder path ${forPath.toString}: ", err)
               originalSender ! LookupFailed
           })
       }
 
-    case CacheProject(forId, result)=>
-      projectsCache ++= Map(forId->result)
+    case CacheProject(forId, result) =>
+      projectsCache ++= Map(forId -> result)
       sender() ! akka.actor.Status.Success
-    case LookupProject(forId)=>
+    case LookupProject(forId) =>
       projectsCache.get(forId) match {
-        case Some(projectRecord)=>
+        case Some(projectRecord) =>
           sender() ! FoundProject(projectRecord)
-        case None=>
+        case None =>
           val originalSender = sender()
           performProjectLookup(forId).onComplete({
-            case Success(record)=>
+            case Success(record) =>
               ownRef ! CacheProject(forId, record)
               originalSender ! FoundProject(record)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could not look up project with ID $forId: ", err)
               originalSender ! LookupFailed
           })
       }
 
-    case CacheCommission(forId, result)=>
-      commissionsCache ++= Map(forId->result)
+    case CacheCommission(forId, result) =>
+      commissionsCache ++= Map(forId -> result)
       sender() ! akka.actor.Status.Success
-    case LookupCommission(forId)=>
+    case LookupCommission(forId) =>
       commissionsCache.get(forId) match {
-        case Some(record)=>
+        case Some(record) =>
           sender() ! FoundCommission(record)
-        case None=>
+        case None =>
           val originalSender = sender()
           performCommissionLookup(forId).onComplete({
-            case Success(record)=>
+            case Success(record) =>
               ownRef ! CacheCommission(forId, record)
               originalSender ! FoundCommission(record)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could no look up commission with ID $forId: ", err)
               originalSender ! LookupFailed
           })
       }
 
-    case CacheWorkingGroups(list, maybeReturn)=>
-      workingGroupCache = list.map(rec=>(rec.uuid->Some(rec))).toMap
+    case CacheWorkingGroups(list, maybeReturn) =>
+      workingGroupCache = list.map(rec => (rec.uuid -> Some(rec))).toMap
       maybeReturn match {
-        case Some(idToReturn)=>
+        case Some(idToReturn) =>
           sender() ! FoundWorkingGroup(workingGroupCache.get(idToReturn).flatten)
-        case None=>
+        case None =>
           sender() ! akka.actor.Status.Success
       }
-    case LookupWorkingGroup(forId)=>
-      if(workingGroupCache.isEmpty){
+    case LookupWorkingGroup(forId) =>
+      if (workingGroupCache.isEmpty) {
         val originalSender = sender()
         performWorkingGroupLookup().onComplete({
-          case Success(maybeRecords)=>
+          case Success(maybeRecords) =>
             //cache the record, perform the lookup on the cached values and then return it to orginalSender
             ownRef.tell(CacheWorkingGroups(maybeRecords.getOrElse(Seq()), Some(forId)), originalSender)
-          case Failure(err)=>
+          case Failure(err) =>
             logger.error(s"Could not look up working groups: ", err)
             originalSender ! LookupFailed
         })
@@ -154,63 +156,76 @@ class PlutoCommunicator(plutoBaseUri:String, plutoUser:String, plutoPass:String)
         sender() ! FoundWorkingGroup(workingGroupCache.get(forId).flatten)
       }
 
-    case CacheMaster(fileName, result)=>
+    case CacheMaster(fileName, result) =>
       masterCache += (fileName -> result)
       sender() ! akka.actor.Status.Success
-    case LookupMaster(fileName)=>
+    case LookupMaster(fileName) =>
       masterCache.get(fileName) match {
-        case Some(cachedRecord)=>
+        case Some(cachedRecord) =>
           sender() ! FoundMaster(cachedRecord)
-        case None=>
+        case None =>
           val originalSender = sender()
           performMasterLookup(fileName).onComplete({
-            case Success(maybeRecords)=>
+            case Success(maybeRecords) =>
               ownRef ! CacheMaster(fileName, maybeRecords)
               originalSender ! FoundMaster(maybeRecords)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could not look up master $fileName: ", err)
               originalSender ! LookupFailed
           })
       }
 
-    case CacheDeliverableAsset(fileName, result)=>
-      deliverableAssetCache += (fileName->result)
+    case CacheDeliverableAsset(fileName, result) =>
+      deliverableAssetCache += (fileName -> result)
       sender() ! akka.actor.Status.Success
-    case LookupDeliverableAsset(fileName)=>
+    case LookupDeliverableAsset(fileName) =>
       deliverableAssetCache.get(fileName) match {
-        case Some(cachedRecord)=>
+        case Some(cachedRecord) =>
           sender() ! FoundDeliverableAsset(cachedRecord)
-        case None=>
+        case None =>
           val originalSender = sender()
           performDeliverableLookup(fileName).onComplete({
-            case Success(maybeRecord)=>
+            case Success(maybeRecord) =>
               ownRef ! CacheDeliverableAsset(fileName, maybeRecord)
               originalSender ! FoundDeliverableAsset(maybeRecord)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could not look up deliverables for $fileName: ", err)
               originalSender ! LookupFailed
           })
       }
 
-    case CacheDeliverableBundle(forId, result)=>
-      deliverableBundleCache += (forId->result)
+    case CacheDeliverableBundle(forId, result) =>
+      deliverableBundleCache += (forId -> result)
       sender() ! akka.actor.Status.Success
-    case LookupDeliverableBundle(forId)=>
+    case LookupDeliverableBundle(forId) =>
       deliverableBundleCache.get(forId) match {
-        case Some(cachedRecord)=>
+        case Some(cachedRecord) =>
           sender() ! FoundDeliverableBundle(cachedRecord)
-        case None=>
+        case None =>
           val originalSender = sender()
           performDelvierableBundleLookup(forId).onComplete({
-            case Success(maybeRecord)=>
+            case Success(maybeRecord) =>
               ownRef ! CacheDeliverableBundle(forId, maybeRecord)
               originalSender ! FoundDeliverableBundle(maybeRecord)
-            case Failure(err)=>
+            case Failure(err) =>
               logger.error(s"Could not look up deliverable bundle with ID $forId: ", err)
               originalSender ! LookupFailed
           })
       }
   }
+}
+
+/**
+  * extract called functions out of the actor class itself so we can call them directly during testing
+  */
+trait PlutoCommunicatorFuncs {
+  protected val logger:org.slf4j.Logger
+  implicit val system:ActorSystem
+  implicit val mat:Materializer
+
+  val plutoBaseUri:String
+  val plutoUser:String
+  val plutoPass:String
 
   /**
     * internal method that consumes a given response entity to a String
--- common/src/test/scala/PlutoCommunicatorSpec.scala
+++ common/src/test/scala/PlutoCommunicatorSpec.scala
@@ -1,8 +1,8 @@
 import java.io.File
 
-import akka.actor.Props
+import akka.actor.{ActorSystem, Props}
 import akka.stream.{ActorMaterializer, Materializer}
-import helpers.PlutoCommunicator
+import helpers.{LocalDateTimeEncoder, PlutoCommunicator, PlutoCommunicatorFuncs}
 import org.specs2.mock.Mockito
 import org.specs2.mutable.Specification
 import akka.http.scaladsl.HttpExt
@@ -14,7 +14,8 @@ import scala.concurrent.{Await, Future}
 import scala.concurrent.ExecutionContext.Implicits.global
 import akka.pattern.ask
 import akka.testkit.TestProbe
-import models.pluto.AssetFolderRecord
+import models.pluto.{AssetFolderRecord, MasterRecord}
+import org.slf4j.LoggerFactory
 
 import scala.concurrent.duration._
 
@@ -83,5 +84,40 @@ class PlutoCommunicatorSpec extends Specification with Mockito {
     }
   }
 
+  "PlutoCommunicatorFuncs ! callToPluto" should {
+    "unmarshal the provided data and return it as a domain object" in new AkkaTestkitSpecs2Support {
+      import io.circe.generic.auto._
+      import LocalDateTimeEncoder._
 
+      val returnContentString =
+        """
+          |[{"user": 520, "title": "Election 2019: On the campaign trail with Boris Johnson and Jeremy Corbyn", "created": "2019-12-10T13:32:47.305", "updated": "2019-12-10T13:33:01.412", "duration": null, "commission": 53046, "project": 53128, "gnm_master_standfirst": null, "gnm_master_website_headline": "Election 2019: On the campaign trail with Boris Johnson and Jeremy Corbyn", "gnm_master_generic_status": null, "gnm_master_generic_intendeduploadplatforms": null, "gnm_master_generic_publish": null, "gnm_master_generic_remove": null}]
+          |""".stripMargin
+
+      implicit val mat:Materializer = ActorMaterializer.create(system)
+      val mockResponseEntity = mock[ResponseEntity]
+      mockResponseEntity.dataBytes returns Source.single(ByteString(returnContentString))
+      val httpMock = mock[HttpExt]
+      httpMock.singleRequest(any,any,any,any) returns Future(HttpResponse(200,entity=mockResponseEntity))
+      val toTest = new PlutoCommunicatorTest(httpMock)
+
+      val req = HttpRequest()
+      val result = Await.result(toTest.callCallToPluto[Seq[MasterRecord]](req), 3 seconds)
+      result must beSome
+      result.get.length mustEqual 1
+      result.get.head.user mustEqual Some(520)
+      result.get.head.title mustEqual "Election 2019: On the campaign trail with Boris Johnson and Jeremy Corbyn"
+    }
+  }
+}
+
+class PlutoCommunicatorTest(httpMock:HttpExt) (override implicit val system: ActorSystem, override val mat: Materializer) extends PlutoCommunicatorFuncs {
+  override val plutoBaseUri: String = "http://localhost:9000"
+  override val plutoPass: String = "nothing"
+  override val plutoUser: String = "nothing"
+  override val logger = LoggerFactory.getLogger("PlutoCommnicatorTest")
+
+  override def callHttp: HttpExt = httpMock
+
+  def callCallToPluto[T:io.circe.Decoder](req:HttpRequest, attempt:Int=1):Future[Option[T]] = callToPluto[T](req, attempt)
 }
